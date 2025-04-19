b7d95c9a1c42a9266a21bc33a4d03e984bed7e2f
kangsan <devstar1223@gmail.com>
2025-04-19 11:21:34 +0900

    Initial Commit

---
+++ src/main/java/com/soundmonster/soundmonster_backend/SoundmonsterBackendApplication.java
@@ -0,0 +1,14 @@
+package com.soundmonster.soundmonster_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundmonsterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundmonsterBackendApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/test/java/com/soundmonster/soundmonster_backend/SoundmonsterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soundmonster.soundmonster_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundmonsterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
