4623c2d727b031435d6f70fbc82d5518ccc1b640
kangsan <devstar1223@gmail.com>
2025-04-19 13:39:16 +0900

    📝 docs(Swagger): Swagger 설정 추가

--- src/main/java/com/soundmonster/soundmonster_backend/domain/user/controller/UserController.java
+++ src/main/java/com/soundmonster/soundmonster_backend/domain/user/controller/UserController.java
@@ -5,6 +5,7 @@ import com.soundmonster.soundmonster_backend.domain.user.dto.PostUsersResponse;
 import com.soundmonster.soundmonster_backend.domain.user.dto.ServicePostUsersResponse;
 import com.soundmonster.soundmonster_backend.domain.user.service.UserService;
 import com.soundmonster.soundmonster_backend.global.dto.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,7 +13,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import static com.soundmonster.soundmonster_backend.domain.user.mapper.UserDtoMapper.*;
+import static com.soundmonster.soundmonster_backend.domain.user.mapper.UserDtoMapper.toPostUsersResponse;
+import static com.soundmonster.soundmonster_backend.domain.user.mapper.UserDtoMapper.toServicePostUsersRequest;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,9 +24,16 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/")
+    @Operation(
+            summary = "이용자 회원가입",
+            description = """    
+                    회원가입 API (추후 설명 추가)
+                    """
+//            security = @SecurityRequirement(name = "JWT")
+    )
     public ResponseEntity<ApiResponse<PostUsersResponse>> postUsers(@RequestBody PostUsersRequest request) {
         ServicePostUsersResponse serviceResponse = userService.postUsers(toServicePostUsersRequest(request));
 
-        return ResponseEntity.ok(ApiResponse.ok(toPostUsersResponse(serviceResponse),"회원가입 완료"));
+        return ResponseEntity.ok(ApiResponse.ok(toPostUsersResponse(serviceResponse), "회원가입 완료"));
     }
 }
