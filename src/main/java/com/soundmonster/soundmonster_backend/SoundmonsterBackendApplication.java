efae8490add5d6f7a04974236b90df6c499a0b13
kangsan <devstar1223@gmail.com>
2025-04-19 12:11:41 +0900

    ✨ feat(BaseEntity): 공통 엔터티 생성 및 감사 기능 추가

--- src/main/java/com/soundmonster/soundmonster_backend/SoundmonsterBackendApplication.java
+++ src/main/java/com/soundmonster/soundmonster_backend/SoundmonsterBackendApplication.java
@@ -2,13 +2,15 @@ package com.soundmonster.soundmonster_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SoundmonsterBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SoundmonsterBackendApplication.class, args);
-		System.out.println("Hello World!");
+		System.out.println("\uD83C\uDFB6Soundmonster Backend Application Start");
 	}
 
 }
